eb3f36bc72a249e6553905fd1575728133cef4cb
Wouter Habets <wouterhabets@gmail.com>
2012-11-15 17:42:17 +0100

    webview toegevoegd

--- src/tk/wouterhabets/android/bcinfo/ActivityMain.java
+++ src/tk/wouterhabets/android/bcinfo/ActivityMain.java
@@ -2,15 +2,24 @@ package tk.wouterhabets.android.bcinfo;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class ActivityMain extends Activity {
-	
+
 	private String currentLevel;
-	
+	private final static String NET_URL = "http://www.wouterhabets.tk";
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		startWebView();
 	}
 
+	private void startWebView() {
+		WebView webview = new WebView(this);
+		setContentView(webview);
+		webview.loadUrl(NET_URL);
+	}
 }
