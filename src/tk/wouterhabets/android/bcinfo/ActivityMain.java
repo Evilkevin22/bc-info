896d05011a6feabfa96f48020780652a3c19665e
Wouter Habets <wouterhabets@gmail.com>
2012-11-15 17:29:36 +0100

    currentlevel placeholder

--- src/tk/wouterhabets/android/bcinfo/ActivityMain.java
+++ src/tk/wouterhabets/android/bcinfo/ActivityMain.java
@@ -4,7 +4,9 @@ import android.app.Activity;
 import android.os.Bundle;
 
 public class ActivityMain extends Activity {
-
+	
+	private String currentLevel;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
