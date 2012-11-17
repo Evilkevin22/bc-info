76c6f82b4bb92b1185c3c6e268f1857dabfce391
Wouter Habets <wouterhabets@gmail.com>
2012-11-17 20:33:16 +0100

    menu toegevoegd

--- src/tk/wouterhabets/android/bcinfo/ActivityMain.java
+++ src/tk/wouterhabets/android/bcinfo/ActivityMain.java
@@ -4,6 +4,9 @@ import android.os.Bundle;
 import android.webkit.WebView;
 
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.view.MenuItem;
 
 public class ActivityMain extends SherlockActivity {
 	
@@ -17,9 +20,35 @@ public class ActivityMain extends SherlockActivity {
 		setWebView();
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater mMenuInflater = getSupportMenuInflater();
+		mMenuInflater.inflate(R.menu.activity_main_menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.menu_main_refresh_item:
+			refresh();
+			break;
+		case R.id.menu_main_about_item:
+			break;
+		case android.R.id.home:
+			// Code voor app icoon button hier invullen
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
 	private void setWebView() {
 		WebView webview = (WebView) findViewById(R.id.webView1);
 		webview.loadUrl(NET_URL);
+	}
+	
+	private void refresh() {
+		
 	}
 		
 }
