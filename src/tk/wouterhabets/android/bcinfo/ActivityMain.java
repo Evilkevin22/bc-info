15f3926766982a3f9aaed405fe4ff7d3b5964ba3
Wouter Habets <wouterhabets@gmail.com>
2012-11-18 12:59:31 +0100

    sharedpreferences toegevoegd

--- src/tk/wouterhabets/android/bcinfo/ActivityMain.java
+++ src/tk/wouterhabets/android/bcinfo/ActivityMain.java
@@ -1,5 +1,6 @@
 package tk.wouterhabets.android.bcinfo;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.SpinnerAdapter;
@@ -18,6 +19,7 @@ public class ActivityMain extends SherlockActivity {
 	private final static String MENU_REFRESH = "Vernieuwen";
 	private final static String MENU_SETTINGS = "Instellingen";
 	private final static String MENU_ABOUT = "Over";
+	private final static String PREFERENCES_NAME = "mSharedPreferences";
 
 	TextView tvTitle, tvSummary;
 
@@ -26,9 +28,8 @@ public class ActivityMain extends SherlockActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
+		// actionbar met spinner instellen
 		ActionBar actionbar = getSupportActionBar();
-		actionbar.setTitle(R.string.title_uitval);
-		actionbar.setSubtitle(getSubtitle());
 		actionbar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
 		SpinnerAdapter mSpinnerAdapter = ArrayAdapter
 				.createFromResource(this, R.array.spinner1,
@@ -39,16 +40,34 @@ public class ActivityMain extends SherlockActivity {
 					@Override
 					public boolean onNavigationItemSelected(int itemPosition,
 							long itemId) {
+						// refresh(itemPosition);
+						
 						return false;
 					}
 				});
 
+		// 2 textviews van layout_main.xml inladen
 		tvTitle = (TextView) findViewById(R.id.layout_main_level);
 		tvSummary = (TextView) findViewById(R.id.layout_main_text);
+
+		// currentLevel uit de SharedPreferences halen
+		SharedPreferences settings = getSharedPreferences(PREFERENCES_NAME, 0);
+		currentLevel = settings.getInt("currentLevel", 0);
+	}
+
+	@Override
+	protected void onStop() {
+		// currentLevel opslaan in SharedPreferences
+		SharedPreferences settings = getSharedPreferences(PREFERENCES_NAME, 0);
+		SharedPreferences.Editor editor = settings.edit();
+		editor.putInt("currentLevel", currentLevel);
+		editor.commit();
+		super.onStop();
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
+		// menu op actionbar/menutoets instellen
 		MenuInflater mMenuInflater = getSupportMenuInflater();
 		mMenuInflater.inflate(R.menu.activity_main_menu, menu);
 		return super.onCreateOptionsMenu(menu);
@@ -56,6 +75,7 @@ public class ActivityMain extends SherlockActivity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+		// controleert welk menu item is geselecteerd en voert code uit
 		switch (item.getItemId()) {
 		case R.id.menu_main_refresh_item:
 			refresh(currentLevel);
@@ -70,41 +90,12 @@ public class ActivityMain extends SherlockActivity {
 			Toast.makeText(getApplicationContext(), MENU_ABOUT,
 					Toast.LENGTH_SHORT).show();
 			break;
-		case android.R.id.home:
-			// Code voor app icoon button hier invullen
-			break;
 		}
 		return super.onOptionsItemSelected(item);
 	}
 
 	private void refresh(int level) {
-
+		// placeholder voor vernieuwen
 	}
 
-	private int getSubtitle() {
-		switch (currentLevel) {
-		case 0:
-			return R.string.level_0;
-		case 1:
-			return R.string.level_1;
-		case 2:
-			return R.string.level_2;
-		case 3:
-			return R.string.level_3;
-		case 4:
-			return R.string.level_4;
-		case 5:
-			return R.string.level_5;
-		case 6:
-			return R.string.level_6;
-		case 7:
-			return R.string.level_7;
-		case 8:
-			return R.string.level_8;
-		case 9:
-			return R.string.level_9;
-		default:
-			return R.string.hello_world;
-		}
-	}
 }
