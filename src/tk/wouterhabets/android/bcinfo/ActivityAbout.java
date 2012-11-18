f713bad54a1c973ce0144466b8027130d214d29d
Wouter Habets <wouterhabets@gmail.com>
2012-11-18 22:49:52 +0100

    over activity echt toegevoegd

---
+++ src/tk/wouterhabets/android/bcinfo/ActivityAbout.java
@@ -0,0 +1,15 @@
+package tk.wouterhabets.android.bcinfo;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class ActivityAbout extends SherlockActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_about);
+	}
+
+}
