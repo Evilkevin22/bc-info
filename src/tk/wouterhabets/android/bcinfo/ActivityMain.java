a920c389509118d9752acbb531bb0ccabf42238d
Wouter Habets <wouterhabets@gmail.com>
2012-12-19 21:08:09 +0100

    SlidingMenu lijst toegevoegd ipv spinner in actionbar

--- src/tk/wouterhabets/android/bcinfo/ActivityMain.java
+++ src/tk/wouterhabets/android/bcinfo/ActivityMain.java
@@ -16,6 +16,11 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import com.actionbarsherlock.app.ActionBar;
 import com.actionbarsherlock.view.Menu;
@@ -24,7 +29,8 @@ import com.actionbarsherlock.view.MenuItem;
 import com.slidingmenu.lib.SlidingMenu;
 import com.slidingmenu.lib.app.SlidingActivity;
 
-public class ActivityMain extends SlidingActivity {
+public class ActivityMain extends SlidingActivity implements
+		OnItemClickListener {
 
 	private int currentLevel;
 	private final static String PREFERENCES_NAME = "mSharedPreferences";
@@ -32,31 +38,40 @@ public class ActivityMain extends SlidingActivity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		//xml interface laden
+
+		// xml interface laden
 		setContentView(R.layout.activity_main);
 		setBehindContentView(R.layout.activityslider_main);
+
+		// actionbar instellen
+		ActionBar actionbar = getSupportActionBar();
+		actionbar.setSubtitle("BC Info - Uitval");
 
 		// currentLevel uit de SharedPreferences halen
 		SharedPreferences settings = getSharedPreferences(PREFERENCES_NAME, 0);
 		setLevel(settings.getInt("currentLevel", 0));
 
-		// actionbar instellen
-		ActionBar actionbar = getSupportActionBar();
-		actionbar.setTitle("Level: " + getLevel());
-		actionbar.setSubtitle("BC Info - Uitval");
-		
+		// slidingmenu instellen
 		SlidingMenu menu = getSlidingMenu();
-		
-        menu.setMode(SlidingMenu.LEFT);
-        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
-        menu.setShadowWidthRes(R.dimen.shadow_width);
-        menu.setShadowDrawable(R.drawable.schaduw);
-        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
-        menu.setFadeDegree(0.35f);
-        //menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
-        // menu.setMenu(R.layout.menu);
-		
+		menu.setMode(SlidingMenu.LEFT);
+		menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+		menu.setShadowWidthRes(R.dimen.shadow_width);
+		menu.setShadowDrawable(R.drawable.schaduw);
+		menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+		menu.setFadeDegree(0.35f);
+		ListView lv = (ListView) findViewById(R.id.listView1);
+		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, android.R.id.text1,
+				getResources().getStringArray(R.array.spinner1));
+		lv.setAdapter(adapter);
+		lv.setOnItemClickListener(this);
+	}
+
+	@Override
+	public void onItemClick(AdapterView<?> arg0, View view, int position,
+			long id) {
+		setLevel(position);
+		refresh(position);
 	}
 
 	@Override
@@ -100,6 +115,10 @@ public class ActivityMain extends SlidingActivity {
 
 	public void setLevel(int level) {
 		currentLevel = level;
+		String items[] = getResources().getStringArray(R.array.spinner1);
+		ActionBar ab = getSupportActionBar();
+		ab.setTitle(items[level]);
+
 	}
 
 	public int getLevel() {
@@ -107,26 +126,25 @@ public class ActivityMain extends SlidingActivity {
 	}
 
 	private void refresh(int level) {
-		
-		//refresh thread, zorgt voor het ophalen van de uitval
+
+		// refresh thread, zorgt voor het ophalen van de uitval
 		Thread refreshThread = new Thread() {
 			public void run() {
 				try {
 					URL url1 = new URL("http://wouterhabets.tk/bcinfo.xml");
-					Log.i("ActivityMain", "RSS feed downloaden van http://wouterhabets.tk/bcinfo.xml...");
+					Log.i("ActivityMain",
+							"RSS feed downloaden van http://wouterhabets.tk/bcinfo.xml...");
 					InputSource ic = new InputSource(url1.openStream());
-					
-					
+
 					SAXParserFactory spf = SAXParserFactory.newInstance();
 					SAXParser sp;
 					sp = spf.newSAXParser();
 					XMLReader xr;
 					xr = sp.getXMLReader();
-					xr.setContentHandler(new RSSHandler());		
-					
+					xr.setContentHandler(new RSSHandler());
+
 					Log.i("ActivityMain", "XMLReader starten...");
 					xr.parse(ic);
-					
 
 				} catch (ParserConfigurationException e) {
 					e.printStackTrace();
@@ -136,13 +154,12 @@ public class ActivityMain extends SlidingActivity {
 					e.printStackTrace();
 				} catch (IOException e) {
 					e.printStackTrace();
-				} 
-				
+				}
+
 			}
 		};
 		refreshThread.start();
-		
-		
+
 	}
 
 }
