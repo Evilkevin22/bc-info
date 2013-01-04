7f88dcbfc29f7242a51335df1c7f862a4a6e820b
Wouter Habets <wouterhabets@gmail.com>
2013-01-04 17:11:09 +0100

    uitval downloader en rsshandler subclass

--- src/tk/wouterhabets/android/bcinfo/ActivityMain.java
+++ src/tk/wouterhabets/android/bcinfo/ActivityMain.java
@@ -1,18 +1,27 @@
 package tk.wouterhabets.android.bcinfo;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
 
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.xml.sax.Attributes;
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -72,7 +81,7 @@ public class ActivityMain extends SlidingActivity implements
 		// currentLevel uit de SharedPreferences halen
 		settings = getSharedPreferences(PREFERENCES_NAME, 0);
 		setLevel(settings.getInt("currentLevel", 0));
-		refresh(getLevel());
+		refresh(currentLevel);
 
 		// indien eerste keer gestart, help tekst weergeven
 		if (settings.getBoolean("firstRun", true)) {
@@ -120,7 +129,7 @@ public class ActivityMain extends SlidingActivity implements
 		// currentLevel opslaan in SharedPreferences
 		// en dat de app al een keer is gestart
 		editor = settings.edit();
-		editor.putInt("currentLevel", getLevel());
+		editor.putInt("currentLevel", currentLevel);
 		editor.putBoolean("firstRun", false);
 		editor.commit();
 		super.onStop();
@@ -155,8 +164,9 @@ public class ActivityMain extends SlidingActivity implements
 			toggle();
 			break;
 		case R.id.menu_main_download_item:
-			DownloadThread downloadThread = new DownloadThread(this);
-			downloadThread.run();			
+			// DownloadThread downloadThread = new DownloadThread(this);
+			// downloadThread.run();
+			new UitvalDownload().execute();
 			break;
 		}
 		return super.onOptionsItemSelected(item);
@@ -167,10 +177,6 @@ public class ActivityMain extends SlidingActivity implements
 		String items[] = getResources().getStringArray(R.array.spinner1);
 		TextView tv = (TextView) findViewById(R.id.layout_main_level);
 		tv.setText(items[level]);
-	}
-
-	public int getLevel() {
-		return currentLevel;
 	}
 
 	private void refresh(int level) {
@@ -190,26 +196,138 @@ public class ActivityMain extends SlidingActivity implements
 			levelDescription.setVisibility(View.VISIBLE);
 		}
 
-		try {
-			InputSource ic;
-			ic = new InputSource(openFileInput("uitvalxml"));
-			SAXParserFactory spf = SAXParserFactory.newInstance();
-			SAXParser sp;
-			sp = spf.newSAXParser();
-			XMLReader xr;
-			xr = sp.getXMLReader();
-			xr.setContentHandler(new RSSHandler());
-			xr.parse(ic);
-		} catch (FileNotFoundException e) {
-			Log.e(DEBUG_TAG, "Refresh - FileNotFoundException");
-		} catch (ParserConfigurationException e) {
-			Log.e(DEBUG_TAG, "Refresh - ParserConfigurationException");
-		} catch (SAXException e) {
-			Log.e(DEBUG_TAG, "Refresh - SAXException");
-		} catch (IOException e) {
-			Log.e(DEBUG_TAG, "Refresh - IOException");
-			// e.printStackTrace();
+		if (level >= 2) {
+			try {
+				InputSource ic;
+				ic = new InputSource(openFileInput("uitvalxml"));
+				SAXParserFactory spf = SAXParserFactory.newInstance();
+				SAXParser sp;
+				sp = spf.newSAXParser();
+				XMLReader xr;
+				xr = sp.getXMLReader();
+				xr.setContentHandler(new RSSHandler());
+				xr.parse(ic);
+			} catch (FileNotFoundException e) {
+				Log.e(DEBUG_TAG, "Refresh - FileNotFoundException");
+			} catch (ParserConfigurationException e) {
+				Log.e(DEBUG_TAG, "Refresh - ParserConfigurationException");
+			} catch (SAXException e) {
+				Log.e(DEBUG_TAG, "Refresh - SAXException");
+			} catch (IOException e) {
+				Log.e(DEBUG_TAG, "Refresh - IOException");
+				// e.printStackTrace();
+			}
 		}
-		
+
+	}
+
+	private class UitvalDownload extends AsyncTask<Void, Void, Void> {
+
+		@Override
+		protected Void doInBackground(Void... params) {
+			final String FILE_XML = "uitvalxml";
+			final String XML_URL = "http://wouterhabets.tk/bcinfo.xml";
+			final String DEBUG_TAG = "UitvalDownload";
+			FileOutputStream fos = null;
+
+			try {
+				fos = openFileOutput(FILE_XML, Context.MODE_PRIVATE);
+			} catch (FileNotFoundException e) {
+				Log.e(DEBUG_TAG, "DownloadThread() FileNotFoundException");
+			}
+
+			if (fos != null) {
+				try {
+					new DefaultHttpClient().execute(new HttpGet(XML_URL))
+							.getEntity().writeTo(fos);
+				} catch (ClientProtocolException e1) {
+					Log.e(DEBUG_TAG,
+							"getUitvalFromServer() ClientProtocolException");
+				} catch (IOException e1) {
+					Log.e(DEBUG_TAG, "getUitvalFromServer() I/O exception");
+				}
+
+				try {
+					fos.close();
+				} catch (IOException e) {
+					Log.e(DEBUG_TAG, "close() IOException");
+				}
+			} else {
+				Log.e(DEBUG_TAG, "FileOutputSteam = null");
+			}
+
+			return null;
+		}
+
+		@Override
+		protected void onPostExecute(Void result) {
+			Log.d(DEBUG_TAG, "onPostExe");
+			File file1 = getBaseContext().getFileStreamPath("uitvalxml");
+			if (file1.exists()) {
+				Log.d(DEBUG_TAG, "Bestand bestaat");
+			} else {
+				Log.d(DEBUG_TAG, "Bestand bestaat niet");
+			}
+
+			super.onPostExecute(result);
+		}
+
+	}
+
+	private class RSSHandler extends DefaultHandler {
+
+		StringBuffer chars = new StringBuffer();
+		//private String levelStrings[] = {null, null, "Havo 2", "Havo 3", "Havo BOVENBOUW"};
+		//private int level;
+		private boolean getIt = false;
+
+		//public RSSHandler(int inlevel, TextView tv1, TextView tv2) {
+		//	level = inlevel;
+		//}
+
+		@Override
+		public void startElement(String uri, String localName, String qName,
+				Attributes atts) {
+
+			chars = new StringBuffer();
+			if (localName.equalsIgnoreCase("item")) {
+				
+			}
+		}
+
+		@Override
+		public void endElement(String uri, String localName, String qName)
+				throws SAXException {
+
+			if (localName.equalsIgnoreCase("title")) {
+				Log.d("RSSHandler", "Title gevonden: " + chars.toString());
+				if (chars.toString() == "Havo BOVENBOUW") {
+					getIt = true;
+					Log.i(DEBUG_TAG, "Gevonden!!!!!!!");
+				}
+
+			}
+
+			if (localName.equalsIgnoreCase("description")) {
+				Log.d("RSSHandler", "Description gevonden: " + chars.toString());
+				if (getIt) {
+					Log.i("RSSHandler", "GEVONDEN: " + chars.toString());
+					getIt = false;
+				}
+
+			}
+
+			if (localName.equalsIgnoreCase("item")) {
+				Log.d("RSSHandler", "Item gevonden.");
+
+			}
+
+		}
+
+		@Override
+		public void characters(char ch[], int start, int length) {
+			chars.append(new String(ch, start, length));
+		}
+
 	}
 }
